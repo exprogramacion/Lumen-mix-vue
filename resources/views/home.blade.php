c3a3284fe4dcb9489e1f27e3ceea385a05008e97
Elihu Díaz <exprogramacion@gmail.com>
2018-02-18 17:31:25 -0600

    Prueba funcional

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel - Components VUEjs</title>
+
+        <link rel="stylesheet" type="text/css" href="{{ url('css/app.css') }}">
+    </head>
+    <body>
+        <div id="app">
+            <example-component></example-component>
+        </div>
+        <script type="text/javascript" src="{{ url('js/app.js') }}"></script>
+    </body>
+</html>
